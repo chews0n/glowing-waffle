706be35d827d2218a83ab23235cc607b0c8b1376
chris hewson <chris@resfrac.com>
2021-04-21 13:12:49 -0600

    added some training steps to main in order to train the model from the OGC data

--- glowingwaffle/__main__.py
+++ glowingwaffle/__main__.py
@@ -1,6 +1,11 @@
 import argparse
 import os
+from glowingwaffle.data import ReadData, ScrapeOGC
 
+OGC_URLS = ['https://reports.bcogc.ca/ogc/app001/r/ams_reports/bc_total_production?request=CSV_Y',
+            'https://reports.bcogc.ca/ogc/app001/r/ams_reports/2?request=CSV_N',
+            'https://iris.bcogc.ca/download/hydraulic_fracture_csv.zip',
+            'https://iris.bcogc.ca/download/drill_csv.zip']
 
 def dir_path(path):
     if os.path.isdir(path):
@@ -27,27 +32,40 @@ def parse_arguments():
                                                  'for use with the Montney Formation.')
 
     # add arguments to the parser
-    parser.add_argument("--train", type=str2bool, nargs='?',
+    parser.add_argument("--train", type=str2bool, nargs='?', dest='train',
                         const=True, default=False,
                         help="Run Retraining of the model.")
 
-    parser.add_argument("--predict", type=str2bool, nargs='?',
+    parser.add_argument("--predict", type=str2bool, nargs='?', dest='predict',
                         const=True, default=True,
                         help="Run the prediction mode. This is done after training if this was set to true.")
 
-    parser.add_argument("--model-folder", type=dir_path, nargs='?', default=os.getcwd(),
+    parser.add_argument("--model-folder", type=dir_path, nargs='?', default=os.getcwd(), dest='model_folder',
                         help="Folder to either output the model (if training is set to true) or read the model if "
                              "only running the predictor")
 
-    parser.add_argument("--input-file", type=argparse.FileType('r'), nargs='?', default='input.csv',
+    parser.add_argument("--input-file", type=argparse.FileType('r'), nargs='?', default='input.csv', dest='input_file',
                         help="Input file for the input to the model predictor.")
 
     # parse the arguments
     args = parser.parse_args()
+    return args
 
 
 def main():
-    parse_arguments()
+    args = parse_arguments()
+
+    if args.train:
+        # Download the OGC data from the OGC website
+        ogc_data = ScrapeOGC(folder=args.model_folder, urls=OGC_URLS)
+        ogc_data.download_data_url()
+
+        # instantiate the training data object from readData
+        training_data = ReadData()
+
+        # Pass one argument into the program which is the rel or absolute path to the csv files
+        # TODO: revise the csv folder reading as there are separate files that require separate data frames for OGC
+        training_data.read_csv_folder(folder=args.model_folder)
 
 
 if __name__ == "__main__":
