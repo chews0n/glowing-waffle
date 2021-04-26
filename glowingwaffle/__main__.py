acce6a53748790f5ab66939efd2e1c64aba988c9
chris hewson <chris@resfrac.com>
2021-04-26 10:55:42 -0600

    fix for the setup.py file in order to correctly compile the package

--- glowingwaffle/__main__.py
+++ glowingwaffle/__main__.py
@@ -1,6 +1,6 @@
 import argparse
 import os
-from glowingwaffle.data import ReadData, ScrapeOGC
+from glowingwaffle.data import ScrapeOGC
 
 OGC_URLS = ['https://reports.bcogc.ca/ogc/app001/r/ams_reports/bc_total_production?request=CSV_Y',
             'https://reports.bcogc.ca/ogc/app001/r/ams_reports/2?request=CSV_N',
