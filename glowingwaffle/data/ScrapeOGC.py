6f38b4d8cf7b089093d5c091e790119c8bd32a0f
chris hewson <chris@resfrac.com>
2021-05-12 15:50:00 -0600

    added fracture feature determination into the ScrapeOGC class

--- glowingwaffle/data/ScrapeOGC.py
+++ glowingwaffle/data/ScrapeOGC.py
@@ -21,6 +21,7 @@ class ScrapeOGC:
         self.urls = urls
         self.dataframes_dict = {}
         self.feature_list = None
+        self.removal_list = list()
 
         if folder is not None and not os.path.exists(folder):
             # create a folder if the folder does not currently exist
@@ -150,10 +151,10 @@ class ScrapeOGC:
 
         if column == "":
             WA_HEADER_NAMES = ['Wa Num', 'WA Num', 'Wa_num', 'WA_num', 'WA_NUM', 'WA_Num', 'Wa_Num', 'WA NUM',
-                               'WA Number','Well Authorization Number']
+                               'WA Number', 'Well Authorization Number']
 
             for header_names in self.dataframes_dict[file_name].columns:
-                for wa_names in  WA_HEADER_NAMES:
+                for wa_names in WA_HEADER_NAMES:
                     if header_names == wa_names:
                         column = header_names
                         break
@@ -162,7 +163,8 @@ class ScrapeOGC:
                     break
 
             if column == "":
-                sys.exit(f'Error Occurred, could not find a well authorization header in: {file_name}, please check the file')
+                sys.exit(
+                    f'Error Occurred, could not find a well authorization header in: {file_name}, please check the file')
 
         df = self.dataframes_dict[file_name].loc[self.dataframes_dict[file_name][column].isin(list_of_values)]
 
@@ -205,7 +207,7 @@ class ScrapeOGC:
         df1, _ = self.find_in_data_frames_dict(file_name=total_prod_file, list_of_values=area_code, column='Area Code')
 
         df2, _ = self.find_in_data_frames_dict(file_name=total_prod_file, list_of_values=formation_code,
-                                            column='Formtn Code')
+                                               column='Formtn Code')
 
         df3 = pd.concat([df1, df2])
 
@@ -247,3 +249,97 @@ class ScrapeOGC:
 
             self.feature_list = pd.merge(self.feature_list, filtered_df, how="left", on=['Well Authorization Number'])
 
+    def determine_frac_type(self):
+        """
+        Use the feature list to determine the frac type and add them to the feature_list, then remove values needed
+        for this determination from the feature_list
+
+        Returns
+        -------
+
+        """
+
+        print("Determining FRAC TYPE")
+
+        self.feature_list.loc[
+            self.feature_list['CHARGE TYPE'].isnull() & self.feature_list['CHARGE SIZE (g)'].isnull() &
+            self.feature_list['SHOTS PER METER'].isnull() &
+            self.feature_list['DEGREE OF PHASING'].isnull(), "FRAC TYPE"] = 'Frac Sleeve'
+
+        self.feature_list.loc[self.feature_list['PERF COMMENTS'] == "AbrasiveJet", "FRAC TYPE"] = 'Frac Sleeve'
+        self.feature_list.loc[self.feature_list['PERF COMMENTS'] == "Burst Disc", "FRAC TYPE"] = 'Frac Sleeve'
+        self.feature_list.loc[self.feature_list['PERF COMMENTS'] == "Toe Port", "FRAC TYPE"] = 'Frac Sleeve'
+        self.feature_list.loc[self.feature_list['PERF COMMENTS'] == "Cemented Sleeve", "FRAC TYPE"] = 'Frac Sleeve'
+
+        self.feature_list.loc[self.feature_list['FRAC TYPE'].isnull(), "FRAC TYPE"] = 'Plug and Perf'
+
+        self.removal_list.append('PERF COMMENTS')
+
+    def calc_frac_props(self):
+        """
+
+        Returns
+        -------
+
+        """
+        print("Determining Proppant Total")
+        self.feature_list['Proppant Total'] = self.feature_list['PROPPANT TYPE1 PLACED (t)'] + \
+                                self.feature_list['PROPPANT TYPE2 PLACED (t)'] + \
+                                self.feature_list['PROPPANT TYPE3 PLACED (t)'] + \
+                                self.feature_list['PROPPANT TYPE4 PLACED (t)']
+
+        group = self.feature_list.groupby('Well Authorization Number')
+
+        df2 = pd.DataFrame()
+
+        print("Determining Proppant Total Sum")
+        df2['Proppant Total Sum'] = group.apply(lambda x: sum(x['Proppant Total']))
+
+        print("Determining Lateral Length")
+        df2['Lateral Length'] = group.apply(
+            lambda x: max(x['COMPLTN BASE DEPTH (m)']) - min(x['COMPLTN TOP DEPTH (m)']))
+
+        print("Determining Average Treating Pressure")
+        df2['Average Treating Pressure'] = group.apply(lambda x: x['AVG TREATING PRESSURE (MPa)'].mean())
+
+        print("Determining Frac Gradient (kPa/m)")
+        df2['FRAC GRADIENT (KPa/m)'] = group.apply(lambda x: x['FRAC GRADIENT (KPa/m)'].mean())
+
+        print("Determining Tonnage Per Metre")
+        df2['Tonnage per m'] = df2['Proppant Total Sum'] / df2['Lateral Length']
+        df2['Tonnage per m'] = df2['Tonnage per m'].round(2)
+
+        print("Determining Energzier")
+        df2['Energizer'] = group.apply(lambda x: x['ENERGIZER'].value_counts().index.tolist()[0] if len(x['ENERGIZER'].value_counts().index.tolist()) > 0 else "None")
+        df2['Energizer Type'] = group.apply(lambda x: x['ENERGIZER TYPE'].value_counts().index.tolist()[0] if len(x['ENERGIZER TYPE'].value_counts().index.tolist()) > 0 else "None")
+
+        print("Determining CO2 Pumped (m3)")
+        df2['Total CO2 Pumped (m3)'] = group.apply(lambda x: sum(x['TOTAL CO2 PUMPED (m3)']))
+
+        print("Determining N2 Pumped (scm)")
+        df2['Total N2 Pumped (scm)'] = group.apply(lambda x: sum(x['TOTAL N2 PUMPED (scm)']))
+
+        print("Determining Total CH4 Pumped (e3m3)")
+        df2['Total CH4 Pumped (e3m3)'] = group.apply(lambda x: sum(x['TOTAL CH4 PUMPED (e3m3)']))
+        df2 = df2.reset_index()
+
+        df2 = df2.drop(['Proppant Total Sum', 'Lateral Length'], axis=1)
+
+        self.feature_list = pd.merge(self.feature_list, df2, how="left", on=['Well Authorization Number'])
+
+    def fill_feature_list_nan_with_val(self, columns=list(), val=0):
+        """
+        fill the columns in the list with the value given instead of nan
+
+        Parameters
+        ----------
+        columns
+        val
+
+        Returns
+        -------
+
+        """
+
+        for column in columns:
+            self.feature_list[column] = self.feature_list[column].fillna(value=0, inplace=True)
--- test/ogcdownloadtest.py
+++ test/ogcdownloadtest.py
@@ -12,7 +12,16 @@ AREA_CODE = [6200, 9022, 9021]
 
 FORMATION_CODE = [4990, 4995, 4997, 5000, 4000]
 
-FILE_DICT = {'wells.csv': ["Surf Nad83 Lat", "Surf Nad83 Long"]}
+FILE_DICT = {'wells.csv': ["Surf Nad83 Lat", "Surf Nad83 Long"],
+             "perf.csv": ['PERF STAGE NUM', 'CHARGE TYPE', 'CHARGE SIZE (g)', 'SHOTS PER METER', 'DEGREE OF PHASING',
+                          'PERF COMMENTS'],
+             'hydraulic_fracture.csv': ['COMPLTN TOP DEPTH (m)', 'COMPLTN BASE DEPTH (m)', 'FRAC STAGE NUM',
+                                        'VISCOSITY GEL TYPE', 'ENERGIZER', 'ENERGIZER TYPE',
+                                        'AVG TREATING PRESSURE (MPa)', 'FRAC GRADIENT (KPa/m)','TOTAL FLUID PUMPED (m3)'
+                                        ,'TOTAL CO2 PUMPED (m3)', 'TOTAL N2 PUMPED (scm)','TOTAL CH4 PUMPED (e3m3)',
+                                        'PROPPANT TYPE1','PROPPANT TYPE1 PLACED (t)','PROPPANT TYPE2',
+                                        'PROPPANT TYPE2 PLACED (t)', 'PROPPANT TYPE3','PROPPANT TYPE3 PLACED (t)',
+                                        'PROPPANT TYPE4','PROPPANT TYPE4 PLACED (t)']}
 
 if __name__ == "__main__":
     # Download the files from the OGC website
@@ -29,5 +38,15 @@ if __name__ == "__main__":
     ogcData.find_well_names(area_code=AREA_CODE, formation_code=FORMATION_CODE)
 
     ogcData.read_well_data(file_name=FILE_DICT)
+
+    ogcData.determine_frac_type()
+
+    ogcData.fill_feature_list_nan_with_val(columns=['PROPPANT TYPE1 PLACED (t)', 'PROPPANT TYPE2 PLACED (t)',
+                                                    'PROPPANT TYPE3 PLACED (t)', 'PROPPANT TYPE4 PLACED (t)'], val=0)
+
+    ogcData.calc_frac_props()
+
+    ogcData.fill_feature_list_nan_with_val(columns=['Total CO2 Pumped (m3)', 'Total N2 Pumped (scm)',
+                                                    'Total CH4 Pumped (e3m3)'], val=0)
 
     print(f"we found {len(ogcData.wa_num)} well names")
